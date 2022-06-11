0de73f8b52cbb289d2be0159c0ef37aabbd7754c
MindChilled <stoxdestro@gmail.com>
2022-06-12 00:21:36 +0700

    UI and Pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pausemenu;
+    // Start is called before the first frame update
+    public void Pause()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Home(int SceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneID);
+    }
+
+}
+
